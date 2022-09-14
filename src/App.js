5dde62feeb9f9cd8325142a7eb80da7a806b11b3
aaronkcarpenterlslog <83232953+aaronkcarpenterlslog@users.noreply.github.com>
2022-09-14 12:15:14 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Application Template</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
